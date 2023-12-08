647f5dc8e1987619535c101fd894cabafc552215
Bqckword <linpan0328@gmail.com>
2023-12-08 07:55:14 -0500

    Fixed the dumb issue

--- src/main/java/frc/robot/AutoManager.java
+++ src/main/java/frc/robot/AutoManager.java
@@ -18,35 +18,20 @@ public class AutoManager {
   /**
    * Automatically generates all the autonomous modes from ../pathplanner/autos
    */
-  private final SendableChooser<Command> chooser = AutoBuilder.buildAutoChooser();
+  private final SendableChooser<Command> chooser = AutoBuilder.buildAutoChooser("Do nothing.");
 
   /**
    * Registers all Commands into NamedCommands.
    */
   public AutoManager() {
-//    NamedCommands.registerCommand("Print", new PrintCommand("Hello world!!!"));
+    NamedCommands.registerCommand("Print", new PrintCommand("Hello world!!!"));
     Constants.Tabs.MATCH.add("Autonomous", chooser).withSize(3, 2);
-
-    List<Translation2d> bezierPoints = PathPlannerPath.bezierFromPoses(
-            new Pose2d(1.0, 1.0, Rotation2d.fromDegrees(0)),
-            new Pose2d(3.0, 1.0, Rotation2d.fromDegrees(0)),
-            new Pose2d(5.0, 3.0, Rotation2d.fromDegrees(90))
-    );
-
-// Create the path using the bezier points created above
-    PathPlannerPath path = new PathPlannerPath(
-            bezierPoints,
-            new PathConstraints(3.0, 3.0, 2 * Math.PI, 4 * Math.PI), // The constraints for this path. If using a differential drivetrain, the angular constraints have no effect.
-            new GoalEndState(0.0, Rotation2d.fromDegrees(-90)) // Goal end state. You can set a holonomic rotation here. If using a differential drivetrain, the rotation will have no effect.
-    );
-
-    chooser.addOption("ijoe32qdw", AutoBuilder.followPathWithEvents(path));
   }
 
   /**
-   * Runs the selected autonomous mode.
+   * Schedules the selected autonomous mode.
    */
   public void executeRoutine() {
-    chooser.getSelected().execute();
+    chooser.getSelected().schedule();
   }
 }
